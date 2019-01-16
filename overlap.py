4f42cd372d5e85f7bb13750f172d3ab06f7af5a5
yeashinr <y_rabbi@live.concordia.ca>
2019-01-16 08:45:31 -0500

    fix overlap.py

--- overlap.py
+++ overlap.py
@@ -1,21 +1,15 @@
 def overlap(PQ,AB):
     index = 0
-    output = 'Empty'
+    output = 'Not Overlap'
+    bool_value = False
 
     for x in range(len(PQ)):
-        if PQ[x] > 0 and PQ[x] >0:
-            while index < len(PQ):
-                print('Hello')
-                if PQ[1] <= AB[index] or PQ[0] >= AB[index]:                #(PQ[0] < AB[0] and AB[1] < PQ[1] or AB[0] < PQ[1] and AB[1] > PQ[0])
-                    output = 'Two lines are overlaped'
-                else:
-                     index +=1
+        if PQ[x] > 0 and PQ[x] > 0 :
+            if (PQ[0] < AB[1] and AB[0] < PQ[1]) or (AB[0] < PQ[1] and PQ[0] < AB[1]):
+                output = 'Two lines are overlapped'
         else:
-            while index < len(PQ):
-                if PQ[1] <= AB[index] or PQ[0] >= AB[index]:                #(PQ[0] < AB[0] and AB[1] < PQ[1] or AB[0] < PQ[1] and AB[1] > PQ[0])
-                    output= 'Two lines are overlaped '
-                else:
-                    index +=1
+            if (PQ[1] < AB[0] and AB[1] < PQ[0]) or ( PQ[1] < AB[0]  and PQ[1] < AB[1]):
+                output = 'Two lines are overlapped'
     return output
 
             
@@ -31,7 +25,7 @@ if __name__ == '__main__':
         AB1 = int(input("Please enter First cordinates for Second line:"))
         AB2 = int(input("Please enter Second cordinates for Second line:"))
         output = overlap([PQ1, PQ2],[AB1,AB2])
-        
+        print(output)
     except ValueError:
         print('Only numeric values are allowed')
     except KeyboardInterrupt:
